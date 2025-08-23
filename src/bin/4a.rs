6988b74aec1c97407e067369529731e268ebcf41
Mario Mottl <mario.mottl@pm.me>
2025-08-23 02:17:06 +0200

    watermelon

--- src/bin/4a.rs
+++ src/bin/4a.rs
@@ -1,15 +1,30 @@
 use std::io;
 
+fn solver(input: String) -> String {
+    let x: i32 = input.trim().parse().expect("Input not an integer");
+    if x >= 4 && x % 2 == 0 {
+        "YES".to_string()
+    } else {
+        "NO".to_string()
+    }
+}
+
 fn main() {
     let mut buffer = String::new();
     let stdin = io::stdin();
     stdin
         .read_line(&mut buffer)
         .expect("Couldnt read from stdin");
-    let input_int: u32 = buffer.parse().expect("Couldnt parse input");
-    if input_int % 2 == 0 {
-        println!("YES");
-    } else {
-        println!("NO");
+    println!("{}", solver(buffer));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        let input = "8".to_string();
+        assert_eq!(solver(input), "YES".to_string())
     }
 }
